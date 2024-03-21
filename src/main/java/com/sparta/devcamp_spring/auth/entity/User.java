049ef0a663648d958452616173966eacda14234d
kuma0112 <lsj23871@gmail.com>
2024-03-21 15:13:10 +0900

    feat : User 엔티티 / UserRole 추가

--- src/main/java/com/sparta/devcamp_spring/auth/entity/User.java
+++ src/main/java/com/sparta/devcamp_spring/auth/entity/User.java
@@ -1,4 +1,44 @@
 package com.sparta.devcamp_spring.auth.entity;
 
-public class User {
+import com.sparta.devcamp_spring.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column (length = 50)
+    private String phoneNumber;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    @Column (columnDefinition = "boolean default false")
+    private boolean isPersonalInfoVerified;
+
+    public User(String name, String email, String password, String phoneNumber, UserRole role){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.isPersonalInfoVerified = true;
+    }
 }
