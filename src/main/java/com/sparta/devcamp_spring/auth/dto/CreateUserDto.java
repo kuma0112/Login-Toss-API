8dd9119d65e031b7b500f91cf9b7b2ad5d1a2b6e
kuma0112 <lsj23871@gmail.com>
2024-03-21 16:08:20 +0900

    fix : class명 변경

---
+++ src/main/java/com/sparta/devcamp_spring/auth/dto/CreateUserDto.java
@@ -0,0 +1,13 @@
+package com.sparta.devcamp_spring.auth.dto;
+
+import com.sparta.devcamp_spring.auth.entity.UserRole;
+import lombok.Getter;
+
+@Getter
+public class CreateUserDto {
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private UserRole userRole = UserRole.USER;
+}
--- src/main/java/com/sparta/devcamp_spring/auth/dto/SignupRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.devcamp_spring.auth.dto;
-
-public class SignupRequestDto {
-}
