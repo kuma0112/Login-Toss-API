4ae1c3c5e2a680c6746b61b10726f2c1768beb9a
kuma0112 <lsj23871@gmail.com>
2024-03-22 23:25:04 +0900

    feat : 회원가입 및 로그인 dto 수정

--- src/main/java/com/sparta/devcamp_spring/auth/dto/SignupResponseDto.java
+++ src/main/java/com/sparta/devcamp_spring/auth/dto/SignupResponseDto.java
@@ -10,13 +10,6 @@ public class SignupResponseDto {
     private String email;
     private String phoneNumber;
 
-    public SignupResponseDto(Long id, String name, String email, String phoneNumber){
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
     public SignupResponseDto(User user){
         this.id = user.getId();
         this.name = user.getName();
