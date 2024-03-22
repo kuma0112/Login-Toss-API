131bd8d0b637c9e2932dd804f3b6141cbb214e47
kuma0112 <lsj23871@gmail.com>
2024-03-22 23:23:28 +0900

    feat : access token / refresh token 엔티티 및 refresh token repository 추가

---
+++ src/main/java/com/sparta/devcamp_spring/auth/entity/AccessToken.java
@@ -0,0 +1,31 @@
+package com.sparta.devcamp_spring.auth.entity;
+
+import com.sparta.devcamp_spring.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+public class AccessToken extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String jti;
+
+    @Column
+    private String token;
+
+    @Column
+    private Date expiresAt;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isRevoke;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
