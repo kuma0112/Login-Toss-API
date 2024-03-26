a4654611cd13bb3781086242e435723f1b9ea1dd
kuma0112 <lsj23871@gmail.com>
2024-03-27 00:43:18 +0900

    refactor : used 컬럼 상태 변경 허용 - setter

--- src/main/java/com/sparta/devcamp_spring/payment/entity/IssuedCoupon.java
+++ src/main/java/com/sparta/devcamp_spring/payment/entity/IssuedCoupon.java
@@ -4,6 +4,7 @@ import com.sparta.devcamp_spring.auth.entity.User;
 import com.sparta.devcamp_spring.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
@@ -36,6 +37,7 @@ public class IssuedCoupon extends BaseEntity {
     private Date validUntil;
 
     @Column(columnDefinition = "boolean default false")
+    @Setter
     private boolean isUsed;
 
     @Column(nullable = true)
